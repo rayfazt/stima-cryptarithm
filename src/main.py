8acc85c798a56767f9320f32386a11de55fb0e84
rayfazt <rayfazt@gmail.com>
2021-01-21 16:18:48 +0700

    starter

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import time
+
+class FileHandler():
+    def __init__(self):
+        pass
+
+    def readFile(self, filename):
+        f = open(filename, "r")
+
+
+class Cryptarithm():
+    def __init__(self):
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
