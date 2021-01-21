c6313758aa283692a91c5406b749f35cced151ec
rayfazt <rayfazt@gmail.com>
2021-01-22 02:09:58 +0700

    asd

--- src/main.py
+++ src/main.py
@@ -1,19 +1,12 @@
-import time
+from time import time
 
-class FileHandler():
-    def __init__(self):
-        pass
-
-    def readFile(self, filename):
-        f = open(filename, "r")
-
-
-class Cryptarithm():
-    def __init__(self):
+def read_file(filename):
+    f = open(filename, 'r')
 
 
 def main():
-
+    tc = input("Masukkan test case: ")
+    read_file(tc)
 
 if __name__ == "__main__":
     main()
