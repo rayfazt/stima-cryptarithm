0c74a9fc252355d6a8afb0824630c6ab43082c77
rayfazt <rayfazt@gmail.com>
2021-01-25 00:49:38 +0700

    semoga kelar

--- src/main.py
+++ src/main.py
@@ -1,21 +1,44 @@
 from time import time
 
+# read from file
 def read_file(filename):
     f = open("../test/" + filename, 'r')
-
-    lines = f.readlines()
-    for i in range(len(lines)):
-        temp_string = lines[i]
-        result_string = ""
-        for j in range(len(temp_string)):
-            if (temp_string[j] != "\n"):
-                result_string += temp_string[j]
-        lines[i] = result_string
-
+    lines = [line.rstrip() for line in f]
     f.close()
+
     return lines
 
+# get first letter of operands
+def get_first_letter(list_string):
+    first = []
+    for i in range(len(list_string) - 2):
+        letter = list_string[i]
+        first.append(letter[0])
+
+    return first
+
+# create list of unique characters without + and -
+def unique_char(list_string):
+    char = []
+    for string in list_string:
+        for letter in string:
+            if letter not in char and letter != "-" and letter != "+":
+                char.append(letter)
+    
+    return char
+
+# convert character to integer
+def to_integer():
+    # to do
+    return 0
 
 if __name__ == "__main__":
     filename = input("Masukkan test case: ")
     input_file = read_file(filename)
+    char = unique_char(input_file)
+    first = get_first_letter(input_file)
+    
+    #list_list = [[char],[0 for i in range(len(char))]]
+    #print(input_file)
+    #print(char)
+    #print(first)
