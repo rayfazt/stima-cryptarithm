5d9d4c92aaafab9a840de40dac392164f2fd008a
rayfazt <rayfazt@gmail.com>
2021-01-27 02:57:00 +0700

    adjustments

--- src/main.py
+++ src/main.py
@@ -40,11 +40,10 @@ def to_integer(string, dict_sol):
             total += str(dict_sol[char])
     return int(total)
 
-# not allowed to use itertools >:( , so let's make one!
-# algorithm taken from python docs with very minor modifications
+# algorithm taken from python docs (https://docs.python.org/3/library/itertools.html#itertools.permutations)
 # Parameters: list_num (list of numbers), r (length of permutation elements)
 # DISCLAIMER: I don't know if this classify as brute force or not, tried to use backtracking and
-#             it somehow performed much worse than this so yeah ¯\_(ツ)_/¯
+#             it somehow performed much worse than this ¯\_(ツ)_/¯
 def permutations(list_num, r):
     pool = tuple(list_num)
     n = len(pool)
@@ -68,14 +67,14 @@ def permutations(list_num, r):
             return
 
 # solver
-# note: can only search for one solution due to formatting and time constraints
+# note: can only search for one solution
 def solve(op, res, char, first, list_num):
     count = 0
     for perm in permutations(list_num, len(char)):
         dict_sol = dict(zip(char, perm)) 
         count += 1
         for num in first:
-            if num != 0:    # first digit of operands can't be 0
+            if dict_sol[num] != 0:    # first digit of operands can't be 0
                 total_op = 0
                 total_res = 0
                 for string in op:
@@ -86,11 +85,9 @@ def solve(op, res, char, first, list_num):
                 # when solution is found, print it
                 if total_op == total_res and len(str(total_res)) == len(res[0]):
                     print("SOLUTION:")
-                    for i in range(len(op)):
-                        if i == len(op) - 1:
-                            print(to_integer(op[i], dict_sol), "+", sep='')
-                        else:
-                            print(to_integer(op[i], dict_sol))
+                    for string in op[:-1]:
+                        print(to_integer(string, dict_sol))
+                    print(to_integer(op[-1], dict_sol), "+", sep='')
                     print("------")
                     print(total_res)
                     return count
@@ -100,6 +97,9 @@ def solve(op, res, char, first, list_num):
 if __name__ == "__main__":
     filename = input("Masukkan test case (dengan .txt): ")
 
+    # begin time
+    start = time.time()
+
     # print problem
     f = open("../test/" + filename, 'r')
     print()
@@ -107,9 +107,6 @@ if __name__ == "__main__":
     print(f.read())
     print()
     f.close()
-
-    # begin time
-    start = time.time()
 
     # initialize parameters
     input_file = read_file(filename)
@@ -122,7 +119,7 @@ if __name__ == "__main__":
     count = solve(op, res, char, first, list_num)
 
     # print performance
-    print("\nWaktu Eksekusi Program:", time.time() - start, "detik")
+    print("\nWaktu Eksekusi Program: {:.2f}".format(time.time() - start), "detik")
     print("Jumlah Total Tes:", count)
 
     print("\nPress enter to exit")
